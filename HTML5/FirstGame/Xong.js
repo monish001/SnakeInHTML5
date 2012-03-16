d86e43dd9b7588a1c8a00dcc474ac5b1d9272e3b
Monish Gupta <monish.gupta1@gmail.com>
2012-03-17 00:10:00 +0530

    HTML5 game structure version 1.0

---
+++ HTML5/FirstGame/Xong.js
@@ -0,0 +1,51 @@
+var _canvas = null;
+var _buffer = null;
+var canvas = null;
+var buffer = null;
+
+function Game(){
+	this.gameLoop = null;
+	var self = this;
+	
+	this.Init = function(){
+		_canvas = document.getElementById('canvas');
+		if (_canvas && _canvas.getContext){
+			canvas = _canvas.getContext('2d');
+			
+			_buffer = document.createElement('canvas');
+			_buffer.width = _canvas.width;
+			_buffer.height = _canvas.height;
+			buffer = _buffer.getContext('2d');
+			
+			buffer.strokeStyle = "rgb(255, 255, 255)";
+			buffer.fillStyle = "rgb(255, 255, 255)";
+			buffer.font = "bold 25px sans-serif";
+		}
+	}
+	
+	this.Run = function(){		
+		if(canvas != null){
+			self.gameLoop = setInterval(self.Loop, 50);
+		}
+			
+	}
+	
+	this.Update = function(){
+		// Update Objects
+	}
+	
+	this.Draw = function(){
+		buffer.clearRect(0, 0, _buffer.width, _buffer.height);
+		canvas.clearRect(0, 0, _canvas.width, _canvas.height);
+		
+		//Draw Code
+		
+		canvas.drawImage(_buffer, 0, 0);
+
+	}
+	
+	this.Loop = function(){
+		self.Update();
+		self.Draw();	
+	}
+}
