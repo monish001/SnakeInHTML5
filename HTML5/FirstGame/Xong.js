03839ddc74bb2e90fa80163fc6f4fafb5f534c67
Monish Gupta <monish.gupta1@gmail.com>
2012-03-21 21:16:31 +0530

    Scope for version 1.0 added + Tentative scope for later versions

--- HTML5/FirstGame/Xong.js
+++ HTML5/FirstGame/Xong.js
@@ -1,3 +1,27 @@
+/*
+ * Author: monish.gupta1@gmail.com
+ * Scope of SNAKE VERSION 1.0 (pathBricks and SnakeHead added)
+ *	1. There is only snakeHead, no snakeTail is present. SnakeHead is extensible to facilitate addition of tail feature of given length in the later versions.
+ *  2. There are only pathBricks present.
+ *  3. Depending on the user input which is given via arrowKeys, the snakeHead changes position on the canvas. 
+ *     It can go through the walls to comeup from the other side.
+ *	4. Depending on the location of the last pathBrick the snakeHead has traversed(NWES), it's head would be shown as directed to the corresponding direction.
+ *  
+ * Scope of SNAKE VERSION 1.1 (1 Maze added)
+ *	1. There will be only one particular maze. 
+ *     It must be written in an extensible fashion so that later(in next versions) on upgrading the stages, differenr mazes could be used.
+ *	2. There is only snakeHead, no snakeTail is present. SnakeHead is extensible to facilitate addition of tail feature of given length in the later versions.
+ *	3. There are two type of bricks pathBricks and mazeBricks.
+ *	4. Depending on the location of the last pathBrick the snakeHead has traversed(NWES), it's head would be shown as directed to the corresponding direction.
+ *  
+ * Scope for SNAKE VERSION 1.2 (Random Generated Food and snakeHead able to consume it)
+ * Scope for SNAKE VERSION 1.3 (Snake Tail added)
+ * Scope for SNAKE VERSION 1.4 (Snake Tail can now grow on eating food)
+ * Scope for SNAKE VERSION 1.5 (mazeBricks dynamic variation feature added)
+ * Scope for SNAKE VERSION 1.6 (power food pluggable feature)
+ *  
+ */
+
 var _canvas = null;
 var _buffer = null;
 var canvas = null;
@@ -9,7 +33,7 @@ function Snake(){
 	
 	//function to update move of snake
 }
-function SnakeMaze(){
+function Maze(){
 	//INFO: board is 800X600 pixels
 	
 	//data structure for maze information
@@ -24,7 +48,7 @@ function SnakeMaze(){
 function Game(){
 	this.gameLoop = null;
 	var self = this;
-	var maze = new SnakeMaze();
+	var maze = new Maze();
 	this.Init = function(){
 		_canvas = document.getElementById('canvas');
 		if (_canvas && _canvas.getContext){
